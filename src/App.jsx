8b1a5e82e2528536e4a522f68318a262c6e06474
Гарбузов Евгений Вячеславович <garbuzov_ev@st.by>
2019-08-30 12:43:39 +0300

    Created toolchain.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+/*todo global tests*/
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']/*,
+        alias: {
+            'react-dom': '@hot-loader/react-dom',
+        }*/
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http://localhost:3000/dist/',
+        hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+};
